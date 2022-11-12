ba04a15e7248c7c65dd8141dfd182c9b45eeb10d
harrizazham98 <harrizazham98@gmail.com>
2022-11-12 08:14:12 +0800

    adding var inside index.ejs

--- exercise/project-express-ssr/routes/index.js
+++ exercise/project-express-ssr/routes/index.js
@@ -33,5 +33,66 @@ router.get("/", function (req, res, next) {
   description: "This page shows a list of products from this manufacturer.",
   });
   });
+
+  var manufacturers = [
+    {
+    id: 1,
+    name: "Lego",
+    imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+    id: 2,
+    name: "Disney",
+    imageUrl: "https://via.placeholder.com/150",
+    },
+    ];
+    var products = [
+    {
+    id: 1,
+    name: "Lego City 2824: Advent Calendar 2010",
+    price: 3.42,
+    imageUrl: "https://via.placeholder.com/150",
+    description: "description placeholder",
+    manufacturerId: 1,
+    },
+    {
+    id: 2,
+    name: "LEGO Friends 41016: Advent Calendar",
+    price: 24.95,
+    imageUrl: "https://via.placeholder.com/150",
+    description: "description placeholder",
+    manufacturerId: 1,
+    },
+    {
+    id: 3,
+    name: "LEGO Star Wars 75018: Jek-14's Stealth Starfighter",
+    price: 68.87,
+    imageUrl: "https://via.placeholder.com/150",
+    description: "description placeholder",
+    manufacturerId: 1,
+    },
+    {
+      id:4,
+      name: "Disney Phineas and Ferb 8 Ferb Plush, soft, cuddle doll toy",
+price: 19.99,
+imageUrl: "https://via.placeholder.com/150",
+description: "description placeholder",
+manufacturerId: 2,
+},
+{
+id: 5,
+name:
+"DESPICABLE ME 2 - Minion cuddly Soft Toy - Plush Figures Banana 28-33 cm, Minion Typ:Bob",
+price: 19.99,
+imageUrl: "https://via.placeholder.com/150",
+description: "description placeholder",
+manufacturerId: 2,
+},
+];
+
+
+    
+
+
   
 module.exports = router;
