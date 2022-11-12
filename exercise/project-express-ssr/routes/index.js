72a91c3d624828a5fb828da56d4cafec073c3a4b
harrizazham98 <harrizazham98@gmail.com>
2022-11-12 09:38:40 +0800

    comment one variable from this array

--- exercise/project-express-ssr/routes/index.js
+++ exercise/project-express-ssr/routes/index.js
@@ -46,15 +46,15 @@ imageUrl: "https://via.placeholder.com/150",
 description: "description placeholder",
 manufacturerId: 2,
 },
-{
-id: 5,
-name:
-"DESPICABLE ME 2 - Minion cuddly Soft Toy - Plush Figures Banana 28-33 cm, Minion Typ:Bob",
-price: 19.99,
-imageUrl: "https://via.placeholder.com/150",
-description: "description placeholder",
-manufacturerId: 2,
-},
+// {
+// id: 5,
+// name:
+// "DESPICABLE ME 2 - Minion cuddly Soft Toy - Plush Figures Banana 28-33 cm, Minion Typ:Bob",
+// price: 19.99,
+// imageUrl: "https://via.placeholder.com/150",
+// description: "description placeholder",
+// manufacturerId: 2,
+// },
 ];
 
 
